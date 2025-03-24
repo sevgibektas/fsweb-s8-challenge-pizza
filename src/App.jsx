ee829702fbacf28a85730bbe867c1f5c9e190465
sevgibektas <sevgibektas61@icloud.com>
2025-03-24 21:01:18 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import workintech from '/workintech.svg'
 import './App.css'
+import { Redirect, Route, Switch } from 'react-router-dom' // `Navigate` yerine `Redirect` kullanıyoruz
+import Order from './pages/Order'
+import Success from './pages/Success'
+import Home from './pages/Home'
+import NavController from './components/NavController'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://github.com/Workintech/fsweb-s7-challenge-pizza" target="_blank">
-          <img src={workintech} className="logo" alt="Workintech logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Workintech + 🍕</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Absolute Acı Pizza sayısı {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Workintech or Pizza logos to learn more
-      </p>
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/order" /> {/* Yönlendirme için Redirect kullanıyoruz */}
+        </Route>
+        <Route path="/order" component={Order} />
+        <Route path="/home" component={Home} />
+        <Route path="/success" component={Success} />
+      </Switch>
+      <NavController />
     </>
   )
 }
---
+++ src/components/NavController.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+const Controller = styled.div`
+position: fixed;
+right: 1rem;
+bottom: 1rem;
+display: flex;
+gap: 1rem;
+`
+
+function NavController() {
+  return (
+    <Controller>
+        <Link to="/">Ana Sayfa</Link>
+        <Link to="/order">Order</Link>
+        <Link to="/home">Home</Link>
+        <Link to="/success">Success</Link>
+    </Controller>
+  )
+}
+
+export default NavController
